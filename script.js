78ad06ad39f9a594d5e0cb989c2f41bb1cb8fe32
Dominique Kromm <dominiquekromm@gmail.com>
2025-05-17 13:35:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -197,16 +197,16 @@ function displayPraktika() {
             main.innerHTML += `
                 <section class = "praktikum">
                     <p style = "text-align: center">Unternehmensname:</p><p style = "color: white"> ${doc.Name}</p>
-                    <p style = "color: #66FCF1">| Adresse: </p><p><a style ="color: white" href = "https://www.google.de/maps/place/${doc.Ort}"><u>${doc.Ort || "Nicht verfügbar"}</u></a></p>
-                    <p style = "color: #66FCF1">| Berufsfeld:</p><p style = "color: white"> ${doc.Berufsfeld || "Nicht verfügbar"}</p>
-                    <p style = "color: #66FCF1">| Beschreibung der Tätigkeit:</p><p style = "color: white"> ${doc.Beschreibung || "Nicht verfügbar"}</p>
-                    <p style = "color: #66FCF1">| Email: </p><p><a style = "color: white" href ="mailto:${doc.Email}"><u>${doc.Email || "Nicht verfügbar"}</u></a></p>
-                    <p style = "color: #66FCF1">| Telefon: </p><p><a style = "color: white" href ="tel:${doc.Tel}"><u>${doc.Tel || "Nicht verfügbar"}</u></a></p>
-                    <p style = "color: #66FCF1">| Link: </p><p> <a style="color: white" href= "${doc.Link}"><u>${doc.Link || "Nicht verführbar"}</u></a></p>
-                    <p style = "color: #66FCF1">| Verfügbare Plätze: </p><p style = "color: white">${doc.AnzahlPlaetze || "Nicht verfügbar"}</p>
-                    <p style = "color: #66FCF1">| Dauer: </p><p style = "color: white"> ${doc.Dauer || "Nicht verführbar"}</p>    
-                    <p style = "color: #66FCF1">| Beginn: </p><p style = "color: white"> ${doc.Beginn || "Nicht verfügbar"}</p>
-                    <p style = "color: #66FCF1">| Letztes Update: </p><p style = "color: white"> ${formattedDateTime || "Nicht verfügbar"}</p>
+                    <p style = "color: #66FCF1">| Adresse: </p><p><a style ="color: white" href = "https://www.google.de/maps/place/${doc.Ort}"><u>${doc.Ort || "Nicht angegeben"}</u></a></p>
+                    <p style = "color: #66FCF1">| Berufsfeld:</p><p style = "color: white"> ${doc.Berufsfeld || "Nicht angegeben"}</p>
+                    <p style = "color: #66FCF1">| Beschreibung der Tätigkeit:</p><p style = "color: white"> ${doc.Beschreibung || "Nicht angegeben"}</p>
+                    <p style = "color: #66FCF1">| Email: </p><p><a style = "color: white" href ="mailto:${doc.Email}"><u>${doc.Email || "Nicht angegeben"}</u></a></p>
+                    <p style = "color: #66FCF1">| Telefon: </p><p><a style = "color: white" href ="tel:${doc.Tel}"><u>${doc.Tel || "Nicht angegeben"}</u></a></p>
+                    <p style = "color: #66FCF1">| Link: </p><p> <a style="color: white" href= "${doc.Link}"><u>${doc.Link || "Nicht angegeben"}</u></a></p>
+                    <p style = "color: #66FCF1">| Verfügbare Plätze: </p><p style = "color: white">${doc.AnzahlPlaetze || "Nicht angegeben"}</p>
+                    <p style = "color: #66FCF1">| Dauer: </p><p style = "color: white"> ${doc.Dauer || "Nicht angegeben"}</p>    
+                    <p style = "color: #66FCF1">| Beginn: </p><p style = "color: white"> ${doc.Beginn || "Nicht angegeben"}</p>
+                    <p style = "color: #66FCF1">| Letztes Update: </p><p style = "color: white"> ${formattedDateTime || "Nicht angegeben"}</p>
                 </section>
             `;
         }
@@ -268,16 +268,16 @@ function showPraktikaFirst(){
             main.innerHTML += `
                 <section class = "praktikum">
                     <p style = "text-align: center">Unternehmensname:</p><p style = "color: white"> ${doc.Name}</p>
-                    <p style = "color: #66FCF1">| Adresse: </p><p><a style ="color: white" href = "https://www.google.de/maps/place/${doc.Ort}"><u>${doc.Ort || "Nicht verfügbar"}</u></a></p>
-                    <p style = "color: #66FCF1">| Berufsfeld:</p><p style = "color: white"> ${doc.Berufsfeld || "Nicht verfügbar"}</p>
-                    <p style = "color: #66FCF1">| Beschreibung der Tätigkeit:</p><p style = "color: white"> ${doc.Beschreibung || "Nicht verfügbar"}</p>
-                    <p style = "color: #66FCF1">| Email: </p><p><a style = "color: white" href ="mailto:${doc.Email}"><u>${doc.Email || "Nicht verfügbar"}</u></a></p>
-                    <p style = "color: #66FCF1">| Telefon: </p><p><a style = "color: white" href ="tel:${doc.Tel}"><u>${doc.Tel || "Nicht verfügbar"}</u></a></p>
-                    <p style = "color: #66FCF1">| Link: </p><p> <a style="color: white" href= "${doc.Link}"><u>${doc.Link || "Nicht verführbar"}</u></a></p>
-                    <p style = "color: #66FCF1">| Verfügbare Plätze: </p><p style = "color: white">${doc.AnzahlPlaetze || "Nicht verfügbar"}</p>
-                    <p style = "color: #66FCF1">| Dauer: </p><p style = "color: white"> ${doc.Dauer || "Nicht verführbar"}</p>    
-                    <p style = "color: #66FCF1">| Beginn: </p><p style = "color: white"> ${doc.Beginn || "Nicht verfügbar"}</p>
-                    <p style = "color: #66FCF1">| Letztes Update: </p><p style = "color: white"> ${formattedDateTime || "Nicht verfügbar"}</p>
+                    <p style = "color: #66FCF1">| Adresse: </p><p><a style ="color: white" href = "https://www.google.de/maps/place/${doc.Ort}"><u>${doc.Ort || "Nicht angegeben"}</u></a></p>
+                    <p style = "color: #66FCF1">| Berufsfeld:</p><p style = "color: white"> ${doc.Berufsfeld || "Nicht angegeben"}</p>
+                    <p style = "color: #66FCF1">| Beschreibung der Tätigkeit:</p><p style = "color: white"> ${doc.Beschreibung || "Nicht angegeben"}</p>
+                    <p style = "color: #66FCF1">| Email: </p><p><a style = "color: white" href ="mailto:${doc.Email}"><u>${doc.Email || "Nicht angegeben"}</u></a></p>
+                    <p style = "color: #66FCF1">| Telefon: </p><p><a style = "color: white" href ="tel:${doc.Tel}"><u>${doc.Tel || "Nicht angegeben"}</u></a></p>
+                    <p style = "color: #66FCF1">| Link: </p><p> <a style="color: white" href= "${doc.Link}"><u>${doc.Link || "Nicht angegeben"}</u></a></p>
+                    <p style = "color: #66FCF1">| Verfügbare Plätze: </p><p style = "color: white">${doc.AnzahlPlaetze || "Nicht angegeben"}</p>
+                    <p style = "color: #66FCF1">| Dauer: </p><p style = "color: white"> ${doc.Dauer || "Nicht angegeben"}</p>    
+                    <p style = "color: #66FCF1">| Beginn: </p><p style = "color: white"> ${doc.Beginn || "Nicht angegeben"}</p>
+                    <p style = "color: #66FCF1">| Letztes Update: </p><p style = "color: white"> ${formattedDateTime || "Nicht angegeben"}</p>
                 </section>
             `;
         }
